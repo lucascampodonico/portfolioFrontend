b94a92384d04f7631cb581eb5f5b497c857056ea
steerven <lucasjoelcampodonico@gmail.com>
2023-04-18 15:20:29 -0300

    Modificaciones

--- src/app/componentes/auth/auth.service.ts
+++ src/app/componentes/auth/auth.service.ts
@@ -1,18 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { API_URL } from 'src/config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-constructor() { }
+constructor(private http: HttpClient) { }
 
-  signIn(email: string, password:string){
+  signIn(email: string, password:string):Observable<any>{
+    let data = {
+      email: email,
+      password: password
+      };
 
-    const token = { token: "asdasdasd" };
-
-    return token
+    return this.http.post(API_URL+"auth/login", data)
 
   }
 
